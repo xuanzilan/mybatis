497539e9430149de7f1cee2a8502f1ce6e63ef75
xuanzilan <985333406@qq.com>
2018-05-28 22:07:13 +0800

    curd操作

--- src/main/java/com/xuanzilan/mybatis/dao/IStudentDao.java
+++ src/main/java/com/xuanzilan/mybatis/dao/IStudentDao.java
@@ -2,10 +2,23 @@ package com.xuanzilan.mybatis.dao;
 
 import com.xuanzilan.mybatis.beans.Student;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author dengshaoheng
  */
 public interface IStudentDao {
 
     void insertStudent(Student student);
+    void insertStudentCacheId(Student student);
+
+    void deleteStudentById(int id);
+    void updateStudent(Student student);
+
+    List<Student> selectAllStudents();
+    Map<Integer, Object> selectAllStudentsMap();
+
+    Student selectStudentById(int id);
+    List<Student> selectStudentsByName(String name);
 }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -6,6 +6,9 @@ import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.List;
+import java.util.Map;
+
 public class StudentTest {
     final static Logger logger = Logger.getLogger(StudentTest.class);
     private IStudentDao dao;
@@ -17,9 +20,63 @@ public class StudentTest {
     }
 
     @Test
-    public void testInsert(){
-        logger.info("插入学生记录");
+    public void testInsertStudent(){
+        logger.info("testInsertStudent");
         Student student = new Student("吴七",17,89.3);
+        logger.info("插入前，student="+student);
         dao.insertStudent(student);
+        logger.info("插入后，student="+student);
+    }
+
+    @Test
+    public void insertStudentCacheId(){
+        logger.info("insertStudentCacheId");
+        Student student = new Student("吴七",17,89.3);
+        logger.info("插入前，student="+student);
+        dao.insertStudentCacheId(student);
+        logger.info("插入后，student="+student);
+    }
+
+    @Test
+    public void deleteStudentById(){
+        logger.info("insertStudentCacheId");
+        int id = 17;
+        dao.deleteStudentById(id);
+    }
+
+    @Test
+    public void updateStudent(){
+        logger.info("updateStudent");
+        Student student = new Student("吴老七",17,89.3);
+        student.setId(17);
+        dao.updateStudent(student);
+    }
+
+    @Test
+    public void selectAllStudents(){
+        logger.info("selectAllStudents");
+        List<Student> students = dao.selectAllStudents();
+        logger.info(students.size());
+    }
+
+    @Test
+    public void selectAllStudentsMap(){
+        logger.info("selectAllStudentsMap");
+        Map<Integer, Object> map  = dao.selectAllStudentsMap();
+        logger.info(map.get(16));
+    }
+
+    @Test
+    public void selectStudentById(){
+        logger.info("selectStudentById");
+        Student student  = dao.selectStudentById(16);
+        logger.info("student="+student);
+    }
+
+    @Test
+    public void selectStudentsByName(){
+        logger.info("selectStudentsByName");
+        List<Student> students = dao.selectStudentsByName("吴七");
+        logger.info(students.size());
     }
 }
