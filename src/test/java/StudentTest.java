556daed9ab5606699f45fec241434267a8ed68aa
xuanzilan <985333406@qq.com>
2018-05-29 07:26:30 +0800

    mapper动态代理

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,8 +1,10 @@
 import com.xuanzilan.mybatis.beans.Student;
 import com.xuanzilan.mybatis.dao.IStudentDao;
-import com.xuanzilan.mybatis.dao.StudentDaoImpl;
+import com.xuanzilan.mybatis.utils.DBUtil;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -12,11 +14,20 @@ import java.util.Map;
 public class StudentTest {
     final static Logger logger = Logger.getLogger(StudentTest.class);
     private IStudentDao dao;
+    private SqlSession sqlSession;
 
     @Before
-    public void init(){
+    public void before(){
 //        BasicConfigurator.configure();
-        dao = new StudentDaoImpl();
+        sqlSession = DBUtil.getSqlSession();
+        dao = sqlSession.getMapper(IStudentDao.class);
+    }
+
+    @After
+    public void after(){
+        if(sqlSession!=null){
+            sqlSession.close();
+        }
     }
 
     @Test
@@ -25,6 +36,7 @@ public class StudentTest {
         Student student = new Student("吴七",17,89.3);
         logger.info("插入前，student="+student);
         dao.insertStudent(student);
+        sqlSession.commit();
         logger.info("插入后，student="+student);
     }
 
@@ -34,6 +46,7 @@ public class StudentTest {
         Student student = new Student("吴七",17,89.3);
         logger.info("插入前，student="+student);
         dao.insertStudentCacheId(student);
+        sqlSession.commit();
         logger.info("插入后，student="+student);
     }
 
@@ -42,6 +55,7 @@ public class StudentTest {
         logger.info("insertStudentCacheId");
         int id = 17;
         dao.deleteStudentById(id);
+        sqlSession.commit();
     }
 
     @Test
@@ -50,6 +64,7 @@ public class StudentTest {
         Student student = new Student("吴老七",17,89.3);
         student.setId(17);
         dao.updateStudent(student);
+        sqlSession.commit();
     }
 
     @Test
@@ -59,12 +74,15 @@ public class StudentTest {
         logger.info(students.size());
     }
 
-    @Test
+    /**
+     * 加入动态代理后不能再使用该方法，因为selectAllStudentsMap在impl实现类中实际调用了xml的selectAllStudents方法
+     */
+    /*@Test
     public void selectAllStudentsMap(){
         logger.info("selectAllStudentsMap");
         Map<Integer, Object> map  = dao.selectAllStudentsMap();
         logger.info(map.get(16));
-    }
+    }*/
 
     @Test
     public void selectStudentById(){
